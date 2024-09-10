005074590b4cee205d34c13bce21845a2511875d
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-10 11:00:08 -0400

    Edited solution code after reviewing solutions video

--- battery-charger.py
+++ battery-charger.py
@@ -1,15 +1,20 @@
 #Once battery reaches 50%, it should begin to charge faster, when it hits 80%, it slows down to prevent overcharge
 
 battery = 0
+increment = 1
 
 while battery < 100:
-    if battery <= 80 and battery >= 50:
-        battery += 5
-        print(f"{battery}%")
-    elif battery >= 80 and battery < 100:
-        battery += 0.5
-        print(f"{battery}%")
-    else:
-        battery += 1
-        print(f"{battery}%")
-print("The battery is full!")
+    battery += increment
+    print(f"{battery}%")
+    if battery == 50:
+        increment = 15
+        print(f"Efficiency check: Fast charging enabled.")
+        #print(f"{battery}%")
+    elif battery == 80:
+        increment = 0.5
+        print(f"Efficiency check: Slowing down charge to prevent overcharging.")
+        #print(f"{battery}%")
+    # else:
+    #     battery += 1
+    #     print(f"{battery}%")
+print("The battery is fully charged.")
