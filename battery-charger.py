d871c4543959cdc07817ef706b7e205d931292ba
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-10 10:52:12 -0400

    just added a % symbol after each battry update print

--- battery-charger.py
+++ battery-charger.py
@@ -5,11 +5,11 @@ battery = 0
 while battery < 100:
     if battery <= 80 and battery >= 50:
         battery += 5
-        print(battery)
+        print(f"{battery}%")
     elif battery >= 80 and battery < 100:
         battery += 0.5
-        print(battery)
+        print(f"{battery}%")
     else:
         battery += 1
-        print(battery)
+        print(f"{battery}%")
 print("The battery is full!")
